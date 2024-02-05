b2859be1ad9ca852c9f1afafb353963fd2e1ee26
V1ck <vick980624@gmail.com>
2024-02-05 15:55:02 +0800

    change target file

--- simulation/RTL_sim/scripts/run.tcl
+++ simulation/RTL_sim/scripts/run.tcl
@@ -1,10 +1,6 @@
 #rtl_sim axi_top + debug_tes
-vcs ../../../verilog/tb_for_combine_32.v \
-../../../verilog/axis_top_ver1126_tcb.v \
-../../../verilog/top_tcb_121_16_10.v \
-../../../verilog/layer0_tcb_121x16x10.v \
-../../../verilog/layer2_tcb_121x16x10.v \
-../../../verilog/comparator_30bit.v \
+vcs ../../../verilog/tb_2_3.v \
+../../../verilog/WC.v \
 -R -full64 -kdb -lca +v2k \
 -debug_access+all \
 +define+RTL_SIM | tee ../../../run_all/RTL_sim.log
